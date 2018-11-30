3dce4f9d02f30b31927a7a3b1b3fdc1aea5b1a4b
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2018-11-30 03:48:48 -0300

    new stuff

--- react/beta/public/app.jsx
+++ react/beta/public/app.jsx
@@ -1,19 +1,46 @@
 var Greeter = React.createClass({
+  // usage of Default props 
+  getDefaultProps: function (){
+    return {
+      name: 'React',
+      message: 'Default message'
+    };
+  },
+  onButtonClick: function (e) {
+        e.preventDefault();
+
+        var name = this.refs.name.value;
+
+        alert(name);
+
+  },
   render: function () {
+
+    // usage of props
+    var name = this.props.name;
+    var message = this.props.message;
+  
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is form a component!</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set name</button>
+        </form>
       </div>
     );
+ 
   }
 });
 
 
-
+// var props
+var firstname = 'Andrew';
 ReactDOM.render(
   // we pass the var through React JSX
-  <Greeter/>,
+  <Greeter name={firstname} message="This is a message"/>,
   document.getElementById('app')
 );
 
