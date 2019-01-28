0acd7be9243d20a70c8881d8ea41d10602f4c35f
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-01-28 14:20:58 -0300

    new stuff

--- bootcamp.js
+++ bootcamp.js
@@ -1,11 +1,50 @@
-// file part of the bootcamp js in udemy with andrew meads
+// file part of the bootcamp js in udemy by andrew meads
 // developer by Felipe f.alfonso.go@gmail.com
 
 // no spaces as can't have broken states in a js file 
 
+/*
 let firstname = 'Blabla'
 let lastname = 'ssasa'
 let fulln = firstname + ' ' + lastname
 
 console.log(fulln)
+
+*/
+
+/*
+let x = 11
+let num = x + 1 * 2
+
+console.log(num)
+*/
+
+ 
+let age = 26
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+
+ 
+
+
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+
+
+console.log(percent)
+
+
+
+/* ---- -*/
+
+
+let petName = 'Hal'
+//let petName = 'Spot'
+petName = 'Spot'
+
+console.log(petName)
 
