05f4a3f6bb33a625710dbad6d8e525b71090e8e2
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-01-29 12:42:02 -0300

    new stuff

--- bootcamp.js
+++ bootcamp.js
@@ -48,3 +48,50 @@ petName = 'Spot'
 
 console.log(petName)
 
+let temp = 31
+let isFreezing = temp === 32
+
+console.log(isFreezing)
+
+
+// ----------
+
+/*
+You are in charge of a display advertising program. Your ads are displayed
+on websites all over the internet. You have some CSV input data that counts
+how many times you showed an ad on each individual domain. Every line consists
+of a count and a domain name. It looks like this:
+*/
+
+const counts = [
+    "900,google.com",
+    "60,mail.yahoo.com",
+    "10,mobile.sports.yahoo.com",
+    "40,sports.yahoo.com",
+    "300,yahoo.com",
+    "10,stackoverflow.com",
+    "2,en.wikipedia.org",
+    "1,es.wikipedia.org",
+    "1,mobile.sports"
+  ]
+  
+  function getTotalbyDomain (counts) { 
+      
+      for (let i=0; i < counts.length; i++) {
+          var clicksAndDomain = counts[i].split(','); // ['900', 'google.com']
+          let clicks = clicksAndDomain[3];
+          // console.log(clicks[0]);
+          
+          var IndiceClicks = counts[i].split(','); // ['900', 'google.com']
+          let clicksIndice = IndiceClicks[1];
+          console.log(clicks, clicksIndice);
+          
+          // mobile.sports.yahoo.com => mobile.sports.yahoo.com , sports.yahoo.com , yahoo.com , com
+      }
+  
+  }
+ 
+  
+  // extractDomains("mobile.sports.yahoo.com")// => ['mobile.sports.yahoo.com', 'sports.yahoo.com', 'yahoo.com', 'com']
+  
+  getTotalbyDomain(counts);
