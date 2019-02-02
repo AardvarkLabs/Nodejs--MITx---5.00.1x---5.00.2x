798c90b691bcce4460f8d6d5aba083eec84ad309
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-02-02 20:19:38 -0300

    new stuff

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -11,12 +11,29 @@ greetUser()
 
 
 let square = function (num) {
-    console.log(num)
+   let result = num * num
+   return result
 }
 
 
-square(3)
-square(10)
+let value  = square(3)
+let otherValue = square(10)
+console.log(value)
+console.log(otherValue)
 
 
+let convertFahrenheitToCelsius = function (fahrenheit) {
+
+     let celsius = (fahrenheit - 32) * 5 / 9
+     return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+
+let tempTwo = convertFahrenheitToCelsius(68)
+
+
+ 
+console.log(tempOne)
+console.log(tempTwo)
 
---
+++ functions/undefined-null.js
@@ -0,0 +1,10 @@
+let name 
+
+name = 'Jen'
+
+if (name === undefined ) {
+    console.log('Please provide a name')
+ } else {
+     console.log(name)
+ }
+ console.log(name)
