59bca7de0e1064d1d6e24b62b5f88cf52b317ef9
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-02-02 19:39:33 -0300

    new stuff

---
+++ functions/functions-101.js
@@ -0,0 +1,22 @@
+// function - input (arguments), code, output
+
+let greetUser = function () {
+    console.log('welcome to user')
+}
+
+
+greetUser()
+greetUser()
+greetUser()
+
+
+let square = function (num) {
+    console.log(num)
+}
+
+
+square(3)
+square(10)
+
+
+
