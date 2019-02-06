704ceb37d2fd29d2cf7fe3d8f2a4dc86a689521b
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-02-06 17:13:55 -0300

    dev - 00222 - new files from jsbootcamp

---
+++ objects/string-meth.js
@@ -0,0 +1,23 @@
+let name = 'Andrew Mead'
+
+//lenght property
+console.log(name.length)
+
+// Convert to uppercase
+
+console.log(name.toUpperCase())
+
+
+// convert to lower case
+console.log(name.toLowerCase())
+
+
+let password = 'abc123pass098'
+console.log(password.includes('px'))
+
+
+
+// 
+
+
+
