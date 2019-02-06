e05fec089079ef3d2abb791747e17913a8c35f2b
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2019-02-05 23:27:32 -0300

    new files to load

--- objects/objects-js.js
+++ objects/objects-js.js
@@ -6,3 +6,22 @@ let myBook = {
 }
 
 console.log(myBook.title)
+
+
+console.log(`${myBook.title}  by ${myBook.author}`)
+
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title}  by ${myBook.author}`)
+
+
+let me = {
+    name: 'Andrew',
+    age: 27,
+    location: 'Phill'
+}
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}`)
+me.age = me.age + 1
+console.log(`${me.name} is ${me.age} and lives in ${me.location}`)
