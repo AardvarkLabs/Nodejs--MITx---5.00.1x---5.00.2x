58141af6773e751acf464640a269c2639fec99f0
Felipe <f.alfonso.go@gmail.com>
2019-02-07 01:44:04 -0300

    new lines

---
+++ objects/number-meth.js
@@ -0,0 +1,14 @@
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+// 0 - .9999
+console.log(randomNum)
