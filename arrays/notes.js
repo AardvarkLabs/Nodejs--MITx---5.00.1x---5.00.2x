31b10eacadc736f013dc210e86e61458d4c32e35
Felipe Alfonso González <felipe@freeshell.de>
2019-02-15 04:57:28 -0300

    changes

--- arrays/notes.js
+++ arrays/notes.js
@@ -2,12 +2,18 @@
 
 
 const notes = ['Note 1', 'Note 2', 'Note 3']
-
+/*
 notes.pop()
 notes.push('my new note')
 
 console.log(notes.shift())
 notes.unshift('my first note')
+*/
+
+todos.splice(2, 1)
+
+
+notes.splice(1, 1)
 
 
 console.log(notes.length)
