532d1f34de87007780fe071b1058ab2f2c3de570
Felipe Alfonso González <felipe@freeshell.de>
2019-02-14 22:48:40 -0300

    new lines

---
+++ arrays/todo.js
@@ -0,0 +1,11 @@
+
+
+const todos = ['order cat food', 'clean kitchen', 'to do work', 'exercise']
+
+console.log(`you have ${todos.length} todos!`)
+
+console.log(`you have ${todos[0]}`)
+
+console.log(`you have ${todos[todos.length - 2]}`)
+
+
