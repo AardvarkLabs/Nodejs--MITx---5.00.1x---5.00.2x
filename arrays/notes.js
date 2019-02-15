e90fe8dbaf379548e9f7d6e0ab1c1c6ed945a875
Felipe Alfonso González <felipe@freeshell.de>
2019-02-15 18:55:29 -0300

    searching arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,27 +1,42 @@
 
 
 
-const notes = ['Note 1', 'Note 2', 'Note 3']
-/*
-notes.pop()
-notes.push('my new note')
+const notes = [{
+    title: 'my next t',
+    body: 'i would like to go to spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'work out. eating a bit better'
+}, {
+    title: 'office',
+    body: 'get a new seat'
+}]
 
-console.log(notes.shift())
-notes.unshift('my first note')
-*/
+// console.log(notes.pop())
+// notes.push('My new note')
 
-todos.splice(2, 1)
+// console.log(notes.shift())
+// notes.unshift('My first note')
 
+// notes.splice(1, 1, 'This is the new second item')
 
+// notes[2] = 'This is now the new note 3'
 
-
-notes.splice(1, 1)
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
 
 console.log(notes.length)
-
 console.log(notes)
 
 
 
+console.log(notes.length)
+console.log(notes)
 
+const index = notes.findIndex(function (note, index) {
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
