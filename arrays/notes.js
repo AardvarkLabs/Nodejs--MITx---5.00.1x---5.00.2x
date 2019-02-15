de0956f58da61c72845926dc5802fcd2a1ab2c41
Felipe Alfonso González <felipe@freeshell.de>
2019-02-15 04:02:28 -0300

    unshift()

--- arrays/notes.js
+++ arrays/notes.js
@@ -3,8 +3,17 @@
 
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+notes.pop()
+notes.push('my new note')
+
+console.log(notes.shift())
+notes.unshift('my first note')
+
+
 console.log(notes.length)
 
-console.log(notes.length - 1)
+console.log(notes)
+
+
 
 
