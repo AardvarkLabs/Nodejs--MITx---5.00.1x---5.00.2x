d7e50777ca09a2c61c84dfe02d10c472dbe952db
Felipe Alfonso González <f.alfonso.go@gmail.com>
2019-11-11 01:18:45 -0300

    new stuff

---
+++ j-03-08-advanced-if/basics/ifstate.js
@@ -0,0 +1,28 @@
+
+
+
+let temp = 138
+
+if (temp <= 32) {
+      console.log('it is freezing out there')
+}
+
+if (temp >= 110) {
+    console.log('it is way to be inside of home due the hot')
+}
+
+// challenge area
+
+let age = 6
+
+
+// if 7 or under print message about child message
+
+if (large <= 7){
+    console.log('you eill get it')
+}
+
+
+if (large >= 65){
+    console.log('you eill get it but with a discount')
+}
