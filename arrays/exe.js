3e5f13740b8f5a3f71614ce474aae464f588beac
Felipe Alfonso González <f.alfonso.go@gmail.com>
2019-12-04 01:50:21 -0300

    stuffing stuff in the fucking cue of lines of code in js eternally

--- arrays/exe.js
+++ arrays/exe.js
@@ -3,7 +3,7 @@ Haga un ciclo for de 100 iteraciones que imprima "fizz" en los múltiplos de 3,
 "buzz" en los múltiplos de 5 y "fizzbuzz" en los múltiplos de 3 y 5
 */
 
-/*
+/* 
 
 for (var i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) console.log(i + " fizzbuzz");
@@ -14,7 +14,7 @@ for (var i = 1; i <= 100; i++) {
     }
 }
 
-*/
+ */
 
 
 /*
@@ -30,8 +30,33 @@ la propiedad Array.prototype que representa el prototipo del constructor de Arra
 */
 
 // Extendiendo Array con un método llamado "duplicator".
-Array.prototype.duplicator = function() {
+/* Array.prototype.duplicator = function() {
    // Se utiliza el método "concat".
    return this.concat(this);   
 }
 
+ */
+
+/*  9999 */
+
+/* 
+888 */
+
+// what's the output
+
+// var is hero objt
+var hero = {
+        // name is defined
+        _name : 'John Doe',
+        //secret id holds the id of hero
+        getSecretIdentity: function () {
+                return this._name;
+        }
+};
+
+var StoleSecretIdentity = hero.getSecretIdentity;
+
+// string = nothing = undefined
+console.log(StoleSecretIdentity());
+// get the name defined in the var 
+console.log(hero.getSecretIdentity());
