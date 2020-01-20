97f5828457cad880ba0900b8ba010f521b06e631
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-01-20 20:43:30 -0300

    new stuff

---
+++ n3-06-04-making-http-req/weather-app/app.js
@@ -0,0 +1,12 @@
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/ecd099679a492007358d1ba36b664ebe/37.8267,-122.4233'
+
+/* request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+}) */
+
+request ({url: url}, (error, response) => {
+    console.log(response)
+})
