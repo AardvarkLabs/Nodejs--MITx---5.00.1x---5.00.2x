69f4660b94eb1f0fa7cbdb23e20e8f66e90993eb
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-01-22 01:56:59 -0300

    done

--- axios-1/axios-1.js
+++ axios-1/axios-1.js
@@ -1,10 +1,14 @@
-const axios = require('axios');
 
-axios.get('https://api.nasa.gov/planetary/earth/imagery')
-  .then(response => {
-    // console.log(response.data.url);
-    console.log(response.data.cloud_score);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+
+var axios = require('axios');
+
+axios.all([
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-03'),
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-02')
+]).then(axios.spread((response1, response2) => {
+  console.log(response1.data.url);
+  console.log(response2.data.url);
+})).catch(error => {
+  console.log(error);
+});
+
