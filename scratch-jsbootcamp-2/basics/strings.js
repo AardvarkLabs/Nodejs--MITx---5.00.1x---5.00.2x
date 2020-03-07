705ba09d3e5ce6a73e726341f09b54ffb704be64
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-07 04:47:08 -0300

    new stuff js

---
+++ scratch-jsbootcamp-2/basics/strings.js
@@ -0,0 +1,27 @@
+//
+/*
+let name = 'andrew mead'
+
+console.log(name)
+*/
+/* let firstName = 'andrew'
+let lastName = 'name'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+ */
+
+/*
+city, country, location
+
+output
+
+phidadelphia, us
+
+*/
+
+let city = 'philadelphia'
+let country = 'US'
+let location = city + ' ' + country
+
+console.log(location)
