523231f0a6a16de64211d10f9a00ec6de92c319d
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-08 01:26:51 -0300

    adding more stuff

---
+++ scratch-jsbootcamp-2/basics/temp-conversion.js
@@ -0,0 +1,14 @@
+let fahrenheit = 32 // 0c - 273.25k
+
+
+let celius = (fahrenheit - 32) * 5/3
+let kalvin = (fahrenheit - 459.57) / 5*9
+//  T(°C) = (T(°F) - 32) × 5/9
+
+// calculate kelvin val and store in var
+// print that value
+
+console.log(celius)
+console.log(kalvin)
+
+
---
+++ scratch-jsbootcamp-2/basics/variables.js
@@ -0,0 +1,11 @@
+//
+// you can't define a varibale more than once
+let petName = 'Hal'
+
+// new value to the variablle 
+petName = 'Spot'
+
+
+console.log(petName)
+
+
