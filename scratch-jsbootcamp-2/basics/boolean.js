58f9ae082edc4d36a28b78388baa3d4762248059
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-08 01:47:09 -0300

    adding more stuff

---
+++ scratch-jsbootcamp-2/basics/boolean.js
@@ -0,0 +1,30 @@
+
+// === equal operator
+// !== no equal operator
+// < less than equal
+// > greater than operator
+// <= less or equal to
+// >= greater than or equal to
+
+/* let temp = 32
+
+let isFreezing = temp >  32
+
+console.log(isFreezing) */
+
+// create age set to your age
+// calculate is child
+// calculate is senior
+// print is child value
+// print to senior value
+
+let myAge = 40
+
+let isChild = myAge < 19
+let senior = myAge > 65
+
+console.log(isChild)
+console.log(senior)
+
+
+
