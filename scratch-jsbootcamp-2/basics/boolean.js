666f6a2bfa97d22a89128688f282e9142cd2eafc
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-08 02:39:29 -0300

    advanced booleans

--- scratch-jsbootcamp-2/basics/boolean.js
+++ scratch-jsbootcamp-2/basics/boolean.js
@@ -17,14 +17,33 @@ console.log(isFreezing) */
 // calculate is senior
 // print is child value
 // print to senior value
-
+/* 
 let myAge = 40
 
-let isChild = myAge < 19
-let senior = myAge > 65
+let isChild = myAge <= 19
+let senior = myAge => 65
 
 console.log(isChild)
-console.log(senior)
+console.log(senior) */
 
 
 
+let temp = 180
+
+if (temp <= 32) {
+    console.log('it is freezing outside')
+}
+
+if (temp >= 110) {
+    console.log('it is way too hot outside')
+}
+
+age = 65
+
+if (age <= 7){
+    console.log('the child is under 7 or equal')
+}
+
+if (age >= 65) {
+    console.log('the person is 65 or older')
+}
