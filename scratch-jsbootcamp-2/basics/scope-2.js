a5ed31e7997d87b8bf014735ba7deb4577b35e3d
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-08 03:45:39 -0300

    logical op - var scope

---
+++ scratch-jsbootcamp-2/basics/scope-2.js
@@ -0,0 +1,23 @@
+
+/* 
+global
+local
+local
+local
+ */
+
+ let name = 'Andrew'
+
+if (true) {
+    // let name = 'mike'
+    if (true) {
+        let tname = 'Jane'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
+
+
