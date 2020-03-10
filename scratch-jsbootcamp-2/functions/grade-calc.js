f8abc1365c8e53c0ce597c49f56ee9d9832e63fb
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-10 04:12:48 -0300

    reorganizing

--- scratch-jsbootcamp-2/functions/grade-calc.js
+++ scratch-jsbootcamp-2/functions/grade-calc.js
@@ -1,7 +1,7 @@
 // students score, total possible score
 //15/20 -> you got a C (75%)!
 // A 98-100. B 88-89, C 70-79, D 6069, F 8-59
-
+/* 
 let gradeCalc =  function (score, totalScore) {
     let percent = (score / totalScore) * 100
     let letterGrade = ''
@@ -20,4 +20,32 @@ let gradeCalc =  function (score, totalScore) {
 }
 
 let result = gradeCalc(13, 20)
-console.log(result)
+console.log(result) 
+
+*/
+
+// students score, total possible score
+//15/20 -> you got a C (75%)!
+// A 98-100. B 88-89, C 70-79, D 6069, F 8-59
+
+let studentScore = function(score, total) {
+  let percent = (score / total) * 100;
+  let letterGrade = " ";
+
+  if (percent >= 90) {
+    letterGrade = "A";
+  } else if (percent >= 80) {
+    letterGrade = "B";
+  } else if (percent >= 70) {
+    letterGrade = "C";
+  } else if (percent >= 60) {
+    letterGrade = "D";
+  } else {
+    letterGrade = "F";
+  }
+  return `you got a ${letterGrade} (${percent}%)`;
+ 
+};
+
+let result = studentScore(12, 22);
+console.log(result);
