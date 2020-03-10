3a1c1f46b13e9473c9f171f1907ae48cd59b6064
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-10 06:23:39 -0300

    lala la

--- scratch-jsbootcamp-2/objects/string-methods.js
+++ scratch-jsbootcamp-2/objects/string-methods.js
@@ -2,3 +2,17 @@ let name = 'andrew mead'
 
 // length property
 console.log(name.length)
+
+// convert to upper case
+
+console.log(name.toUpperCase())
+
+console.log(name.toLowerCase())
+
+
+let password = 'abc123sdfsfwer098'
+console.log(password.includes('password'))
+
+
+// trim method
+console.log(name.trim())
