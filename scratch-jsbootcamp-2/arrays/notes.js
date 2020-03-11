78089a7dfa571ee1f603a9be6290e7ae3dd6e812
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-11 04:59:06 -0300

    new stuff about arrays

--- scratch-jsbootcamp-2/arrays/notes.js
+++ scratch-jsbootcamp-2/arrays/notes.js
@@ -1,5 +1,18 @@
 const notes = ['note 1', 'Note 2', 'Note 3']
 
-console.log(notes.length)
+const todos = ['Note 1', 'Note 2', 'Note 3']
+/* 
+console.log(notes.pop())
+notes.push('My new note')
 
-console.log(notes[0])
+console.log(notes.shift())
+notes.unshift('My first note')
+ */
+//notes.splice(1, 1, 'this is a seconfd item')
+
+notes[2] = 'this is a new note 3'
+
+console.log(notes.length)
+console.log(notes)
+
+
--- scratch-jsbootcamp-2/arrays/todo.js
+++ scratch-jsbootcamp-2/arrays/todo.js
@@ -1,25 +1,19 @@
 //arrays with 5 times
 // have a todos
 // print first and the second to olist items -> todo: walk the dog
- 
-const todos = ['read news', 'walk the dog', 'shower', 'drive', 'work']
 
-let firstItem = todos[0]
-let lastItem = todos[[todos.length-1]]
+const todos = ["read news", "walk the dog", "shower", "drive", "work"];
 
-console.log(firstItem)
-console.log(lastItem) 
+console.log(`you have ${todos.length} todos!`);
+let firstItem = todos[0];
+let lastItem = todos[[todos.length - 1]];
+console.log(firstItem);
+console.log(lastItem);
 
-/* 
-
+/*
 following by Andrew mead:
-
 const todos = ['food', 'clean', 'cat', 'shower', 'work out']
-
 console.log(`you have ${todos.length} todos!`)
-
 console.log(`Todo: ${todos[0]}`)
 console.log(`Todo: ${todos[todos.length - 2]}`)
-
-
  */
