20a2bfe9eb7a8af3a58d949e700ff0ea3b9cf608
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-11 03:16:13 -0300

    arrays in depth

--- scratch-jsbootcamp-2/arrays/notes.js
+++ scratch-jsbootcamp-2/arrays/notes.js
@@ -1,3 +1,5 @@
 const notes = ['note 1', 'Note 2', 'Note 3']
 
-console.log(notes)
+console.log(notes.length)
+
+console.log(notes[0])
--- scratch-jsbootcamp-2/arrays/todo.js
+++ scratch-jsbootcamp-2/arrays/todo.js
@@ -1,3 +1,25 @@
-const notes = ['note 1', 'Note 2', 'Note 3']
+//arrays with 5 times
+// have a todos
+// print first and the second to olist items -> todo: walk the dog
+ 
+const todos = ['read news', 'walk the dog', 'shower', 'drive', 'work']
 
-console.log(notes)
+let firstItem = todos[0]
+let lastItem = todos[[todos.length-1]]
+
+console.log(firstItem)
+console.log(lastItem) 
+
+/* 
+
+following by Andrew mead:
+
+const todos = ['food', 'clean', 'cat', 'shower', 'work out']
+
+console.log(`you have ${todos.length} todos!`)
+
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
+
+
+ */
