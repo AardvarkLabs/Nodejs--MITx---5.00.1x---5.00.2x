4ec1871b2dcc918f21f99e559bb89e59fce3e516
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-12 05:13:49 -0300

    ... for && arrays .....

--- scratch-jsbootcamp-2/arrays/notes.js
+++ scratch-jsbootcamp-2/arrays/notes.js
@@ -30,7 +30,26 @@ console.log(notes)
 // counting ... : 1
 // 
 // for (let count = 0; count <= 2; count++) {
-// now: in reverse 
+// now: in reve
+/*
 for (let count = 2; count >= 0; count--) {
     console.log(count)
 }
+
+// for statement with arrays
+
+for (let count = 0; count < notes.length; count++) {
+    console.log(notes[count])
+}
+
+for (let count = notes.length -1; count >=0; count--) {
+    console.log(notes[count])
+}
+ */
+
+ for (let count = 0; count < todos.length; count++) {
+    const num = count + 1
+    const todo =  todos[count]
+    console.log(`${num}, ${todo}`)
+    console.log('----------')
+ }
