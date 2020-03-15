69aff61832ebf0abe9171c93d4724a5856ae7fea
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-14 21:49:00 -0300

    ... ..

--- scratch-jsbootcamp-2/arrays/notes.js
+++ scratch-jsbootcamp-2/arrays/notes.js
@@ -1,6 +1,36 @@
-const notes = ['note 1', 'Note 2', 'Note 3']
+
+//const notes = ['note 1', 'Note 2', 'Note 3']
 
 const todos = ['Note 1', 'Note 2', 'Note 3']
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go Spain'
+}, {
+    title: 'habbit to work on',
+    body: 'Excersive. Eating a bit healthy'
+}, {
+    title: 'Office emodification',
+    body: 'Get a new seat'
+
+}]
+
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+const note = findNote(notes, 'some other office modification')
+console.log(note)
+
 /* 
 console.log(notes.pop())
 notes.push('My new note')
@@ -10,7 +40,7 @@ notes.unshift('My first note')
  */
 //notes.splice(1, 1, 'this is a seconfd item')
 
-notes[2] = 'this is a new note 3'
+/* notes[2] = 'this is a new note 3'
 
 notes.forEach(function(item, index) {
     console.log(index)
@@ -18,10 +48,31 @@ notes.forEach(function(item, index) {
 })
 
 
+ */
 
-
-console.log(notes.length)
+/* console.log(notes.length)
 console.log(notes)
+ */
+//console.log(notes.indexOf({}))
+
+/* console.log(notes.length)
+console.log(notes)
+ */
+/*
+let someObject = {}
+let otherObject = someObject
+
+console.log(someObject == someObject)
+
+ */
+
+const index = notes.findIndex(function (note, index) {
+    console.log(note)
+    return note.title == 'habbit to work on'
+
+})
+
+console.log(index)
 
 
 
@@ -46,7 +97,7 @@ for (let count = notes.length -1; count >=0; count--) {
     console.log(notes[count])
 }
  */
-
+/* 
  for (let count = 0; count < todos.length; count++) {
     const num = count + 1
     const todo =  todos[count]
@@ -54,4 +105,5 @@ for (let count = notes.length -1; count >=0; count--) {
     console.log('----------')
  }
 
- 
+  */
+
