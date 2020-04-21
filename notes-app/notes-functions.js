75cd9afc97d029cab0d3b65ba919d71c6f3b0756
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-04-21 05:56:55 -0400

    adding a few lines

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -1,8 +1,3 @@
-// updates
-
-console.log(uuidv4())
-
-
 // Read existing notes from localStorage
 const getSavedNotes = function () {
     const notesJSON = localStorage.getItem('notes')
@@ -19,14 +14,17 @@ const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
-const removeNote = = function (id) {
-	const noteIndex = notes.findIndex(funcyion(note){
-		return note.id === id
-	})
-	if (noteIndex > -1) {
-		ntoes.splice(noteIndex, 1)
-	}
+// Remove a note from the list
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
 }
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
@@ -35,18 +33,12 @@ const generateNoteDOM = function (note) {
 
     // Setup the remove note button
     button.textContent = 'x'
-    // noteEl.appendChild(button)
-	noteEl.appendChild(button)
-	
-	button.addEventListener('click', function () {
-		// console.log(note)
-		// for later 6.51 AM
-	
-		removeNote(note.id)
-			renderNotes(notes, filters)
-
-	
-	})
+    noteEl.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
 
     // Setup the note title text
     if (note.title.length > 0) {
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,9 +1,6 @@
-// Fetch existing todos from localStorage
-const getSavedTodos = function () {
-    const todosJSON = localStorage.getItem('todos')
 
-    if (todosJSON !== null) {
-        return JSON
+
+// Fetch existing todos from localStorage
 const getSavedTodos = function () {
     const todosJSON = localStorage.getItem('todos')
 
@@ -19,60 +16,15 @@ const saveTodos = function (todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
-// Render application todos based on filters
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        
-        return searchTextMatch && hideCompletedMatch
+// Remove todo by id
+const removeTodo = function (id) {
+    const todoIndex = todos.findIndex(function (todo) {
+        return todo.id === id
     })
 
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
-
-    filteredTodos.forEach(function (todo) {
-        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
-    })
-}
-
-// Get the DOM elements for an individual note
-const generateTodoDOM = function (todo) {
-    const todoEl = document.createElement('div')
-    const checkbox = document.createElement('input')
-    const todoText = document.createElement('span')
-    const removeButton = document.createElement('button')
-
-    // Setup todo checkbox
-    checkbox.setAttribute('type', 'checkbox')
-    todoEl.appendChild(checkbox)
-
-    // Setup the todo text
-    todoText.textContent = todo.text
-    todoEl.appendChild(todoText)
-
-    // Setup the remove button
-    removeButton.textContent = 'x'
-    todoEl.appendChild(removeButton)
-
-    return todoEl
-}
-
-// Get the DOM elements for list summary
-const generateSummaryDOM = function (incompleteTodos) {
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    return summary
-}
-}
-
-// Save todos to localStorage
-const saveTodos = function (todos) {
-    localStorage.setItem('todos', JSON.stringify(todos))
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
 }
 
 // Render application todos based on filters
@@ -105,6 +57,10 @@ const generateTodoDOM = function (todo) {
 
     // Setup todo checkbox
     checkbox.setAttribute('type', 'checkbox')
+
+    // new stuff
+    checkbox.checked = todo.completed
+
     todoEl.appendChild(checkbox)
 
     // Setup the todo text
@@ -112,8 +68,26 @@ const generateTodoDOM = function (todo) {
     todoEl.appendChild(todoText)
 
     // Setup the remove button
+/*   
+      removeButton.textContent = 'x'
+      todoEl.appendChild(removeButton)
+      removeButton.addEventListener('click', function () {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    }) 
+    
+*/
+
+    // setup the remove button as exercise.
+
     removeButton.textContent = 'x'
     todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+        removeTodo(todo-id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
 
     return todoEl
 }
