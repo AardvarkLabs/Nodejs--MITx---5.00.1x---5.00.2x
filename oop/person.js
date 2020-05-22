7c0510614307ea066c61fef22fecb0e5514f46a5
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-05-22 04:44:37 -0400

    construtors in js - oop

--- oop/person.js
+++ oop/person.js
@@ -10,7 +10,7 @@ console.log(me)
 const person2 = new Person('Clancey', 'Turner', 51)
 console.log(person2) */
 
-const Person = function (firstName) {
+const Person = function (firstName, lastName, age) {
     this.firstName = firstName
 	this.lastName = lastName
 	this.age = age
