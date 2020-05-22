d742e0d3ac665920e814121ad99bd501cdb685f6
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-05-22 04:32:34 -0400

    v2.3.4

--- oop/person.js
+++ oop/person.js
@@ -1,4 +1,4 @@
-const Person = function (firstName, lastName, age) {
+/* const Person = function (firstName, lastName, age) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
@@ -8,4 +8,20 @@ const me = new Person('Andrew', 'Mead', 27)
 console.log(me)
 
 const person2 = new Person('Clancey', 'Turner', 51)
+console.log(person2) */
+
+const Person = function (firstName) {
+    this.firstName = firstName
+	this.lastName = lastName
+	this.age = age
+};
+
+const me = new Person('Andrew', 'Mead', 27);
+
+console.log(me);
+
+
+const person2 = new Person('Clancey', 'Turner', 51)
+
 console.log(person2)
+
