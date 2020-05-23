13c9890ca75f2e5fcc9a5c470ea742596f8767ab
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-05-23 03:34:16 -0400

    2.00.001

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,3 +1,4 @@
+/*
 const Hangman = function (word, remainingGuesses) {
     this.word = word
     this.remainingGuesses = remainingGuesses
@@ -8,3 +9,21 @@ console.log(game1)
 
 const game2 = new Hangman('New Jersey', 4)
 console.log(game2)
+*/
+
+// prototypol inheritance 
+
+const Hangman = function (word, remaningGuesses) {
+	this.word = word
+	this.remainingGuesses = remainingGuesses
+}
+
+
+const game1 = new Hangman('cat', 2)
+console.log(game2)
+
+const game2 = new Hangman('New Jersey', 4)
+console.log(game2)
+/*
+
+----*/
--- oop/person.js
+++ oop/person.js
@@ -1,4 +1,5 @@
-/* const Person = function (firstName, lastName, age) {
+/* 
+const Person = function (firstName, lastName, age) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
@@ -8,20 +9,29 @@ const me = new Person('Andrew', 'Mead', 27)
 console.log(me)
 
 const person2 = new Person('Clancey', 'Turner', 51)
-console.log(person2) */
+console.log(person2) 
+*/
 
 const Person = function (firstName, lastName, age) {
-    this.firstName = firstName
-	this.lastName = lastName
-	this.age = age
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
 };
 
-const me = new Person('Andrew', 'Mead', 27);
+Person.prototype.getBio = function () {
+  return `${this.firstName} is ${this.age}.`;
+};
 
-console.log(me);
+Person.prototype.setName = function (full) {
+  const names = fullName.split(' ');
+  this.firstName = names[0]
+  this.lastName = names[1]
+};
 
+const me = new Person("Andrew", "Mead", 27);
+me.setName("Aleis Turner");
 
-const person2 = new Person('Clancey', 'Turner', 51)
+console.log(me.getBio());
 
-console.log(person2)
-
+const person2 = new Person("Clancey", "Turner", 51);
+console.log(person2);
