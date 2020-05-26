99963874a6bef13d067c61c923cd9cf8643d2d62
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-05-26 03:53:52 -0400

    v4.0001

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,48 +1,25 @@
-/*
 const Hangman = function (word, remainingGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c', 'e']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
 }
 
 const game1 = new Hangman('Cat', 2)
-console.log(game1)
+console.log(game1.getPuzzle())
 
 const game2 = new Hangman('New Jersey', 4)
-console.log(game2)
-*/
-
-// prototypol inheritance 
-
-const Hangman = function (word, remaningGuesses) {
-	this.word = word.toLowerCase().split('')
-	this.remainingGuesses = remainingGuesses
-	this.guessedLetters = ['c']
-}
-
-// half part of the code.
-Hangman.prototype.getPuzzle = function () {
-	let puzzle = ''
-	
-	this.word.forEach((letter) => {
-		if (this.guessedLetters.includes(letter)) {
-			
-		} else {
-			puzzle = puzzle + '*'
-		}
-	})
-	
-	return puzzle
-}
-
-const game1 = new Hangman('cat', 2)
-console.log(game1)
-
-const game2 = new Hangman('New Jersey', 4)
-console.log(game2)
-
-
-/*
-
-----
-
-*/
+console.log(game2.getPuzzle())
