6abf0b6d661f12d70d3b97e88160f173b8a91f72
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-06-13 02:46:06 -0400

    few lines

--- oop/get-set.js
+++ oop/get-set.js
@@ -19,3 +19,25 @@ console.log(data)
 data.location = ' Philla '
 data.location = ' New Y '
 console.log(data)
+
+// training in js.
+
+class Student extends Person {
+	constructor(firstName, lastName, age, grade, likes) {
+		super(firstName, lastName, age, likes)
+		this.grade = grade
+	}
+	updateGrade(change) ´
+	this.grade += change
+	getBio() {
+		const status = this.grade >= 70 ? 'passing' : 'failing'
+		return `${this.firstName} is ${status} the class`
+	}
+}
+
+const me = new Student('Andrew', 'Mead', 27, 88, [])
+me.fullName = 'Clancey Turner'
+console.log(me.getBio())
+me.updateGrade(-20)
+console.log(me.getBio())
+
