54e4ff17bef79ce695114660604177ca686ed925
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-07-06 01:46:14 -0400

    added recently offline

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,11 @@
+
+// http request . transfer protocol 
+// request 
+// response 
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cat', 2)
+const game1 = new Hangman('Car parts', 2)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
@@ -11,3 +16,17 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// http request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState === 4) {
+		const data = JSON.parse(e.target.responseText)
+		console.log(datas)
+	}
+})
+
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
